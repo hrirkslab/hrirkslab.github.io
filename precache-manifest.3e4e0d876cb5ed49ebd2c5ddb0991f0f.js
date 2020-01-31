2a57de9368e4a437682250f51a2f0965e4fa4578
hrirks <hrirkslab@gmail.com>
2020-01-31 01:49:27 +0100

    Updates

---
+++ precache-manifest.3e4e0d876cb5ed49ebd2c5ddb0991f0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c652bfa75fa88aceb3195040140c41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22887a1a5e3aa13c8992",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b71c5ed71eea038b7bc8",
+    "url": "/static/js/2.d7cb2a6e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d7cb2a6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22887a1a5e3aa13c8992",
+    "url": "/static/js/main.3f9b739a.chunk.js"
+  },
+  {
+    "revision": "45121b566087e8a77cda",
+    "url": "/static/js/runtime-main.32bdd71a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
